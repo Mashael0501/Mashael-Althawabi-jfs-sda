b9b4070267d72e75c5f7211bdb8e41da6e4938e5
Mashael0501 <mashael_15@outlook.com>
2021-07-20 05:31:08 +0300

    lession1 complete

---
+++ phase1/lesson1/simple-project/src/com/MyException.java
@@ -0,0 +1,10 @@
+package com;
+
+public class MyException {
+
+}
+//class MyException extends Exception {}
+
+//if (condition){
+     //throw new MyException();
+//}
---
+++ phase1/lesson1/simple-project/src/com/Student.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Student extends person {
+	
+	String usn;
+	String grade;
+	void updareGrade() {
+		System.out.println("updareGrade () in Student");
+	}
+
+}
